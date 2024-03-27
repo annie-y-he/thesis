3de302bced43abdf3fbf3eab70e7be4e3f9e6cb8
Annie He <anniehe1126@gmail.com>
2024-03-27 19:57:36 -0400

    docker compose no dash

--- docker.sh
+++ docker.sh
@@ -5,12 +5,12 @@ PROJECT_NAME="thesis"
 
 if [ "$1" = "rm" ]; then
   echo "Stopping and removing containers..."
-  docker-compose -p $PROJECT_NAME down -v
+  docker compose -p $PROJECT_NAME down -v
 elif [ $1 = "app" ]; then
   docker exec -it ${PROJECT_NAME}-app-1 bash
 else
   echo "Starting services..."
-  docker-compose -p $PROJECT_NAME up -d
+  docker compose -p $PROJECT_NAME up -d
   echo "Containers are running in the background."
 fi
 
