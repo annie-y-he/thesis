a97ad02b0f99f33d090a434125ddc9580102116b
Annie He <anniehe1126@gmail.com>
2024-03-28 06:39:41 -0400

    sudo

--- docker.sh
+++ docker.sh
@@ -5,23 +5,23 @@ PROJECT_NAME="thesis"
 
 if [ "$1" = "rm" ]; then
   echo "Stopping and removing containers..."
-  docker compose -p $PROJECT_NAME down -v
+  sudo docker compose -p $PROJECT_NAME down -v
 elif [ "$1" = "app" ]; then
-  docker exec -it ${PROJECT_NAME}-app-1 bash
+  sudo docker exec -it ${PROJECT_NAME}-app-1 bash
 elif [ "$1" = "restart" ]; then
   echo "Pulling latest changes from Git..."
   git pull
   echo "Rebuilding and restarting services..."
-  docker compose -p $PROJECT_NAME up -d --build
+  sudo docker compose -p $PROJECT_NAME up -d --build
   echo "Installing dependencies..."
-  docker exec ${PROJECT_NAME}-app-1 npm install
+  sudo docker exec ${PROJECT_NAME}-app-1 npm install
   echo "Building the application..."
-  docker exec ${PROJECT_NAME}-app-1 npm run build
-  echo "Restarting docker..."
-  docker compose -p $PROJECT_NAME up -d --build
+  sudo docker exec ${PROJECT_NAME}-app-1 npm run build
+  echo "Restarting container..."
+  sudo docker compose -p $PROJECT_NAME up -d --build
 else
   echo "Starting services..."
-  docker compose -p $PROJECT_NAME up -d
+  sudo docker compose -p $PROJECT_NAME up -d
   echo "Containers are running in the background."
 fi
 
