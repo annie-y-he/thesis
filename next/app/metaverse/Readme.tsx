14880b7e434d77e49e6b4be531dc35ae667f652f
Annie He <anniehe1126@gmail.com>
2024-03-29 05:48:58 -0400

    gitpub

---
+++ next/app/metaverse/Readme.tsx
@@ -0,0 +1,59 @@
+import s from './page.module.scss'
+
+export default function Readme() {
+  return (
+    <div className={s.md}>
+      <h1>ECE150: Digital Logic Design</h1>
+      <br />
+      <h4>Final Project</h4>
+      <hr />
+      <h3>Overview</h3>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui nobis quasi illum et expedita quia autem sequi magni impedit eligendi corrupti nemo iusto culpa consequatur, nesciunt quod suscipit adipisci, quae repudiandae! Adipisci, ratione? Similique doloribus accusamus quod porro temporibus perspiciatis id velit, molestiae, sint dolore natus soluta alias labore laudantium!</p>
+      <hr />
+      <h3>Table of Contents</h3>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui nobis quasi illum et expedita quia autem sequi magni impedit eligendi corrupti nemo iusto culpa consequatur, nesciunt quod suscipit adipisci, quae repudiandae! Adipisci, ratione? Similique doloribus accusamus quod porro temporibus perspiciatis id velit, molestiae, sint dolore natus soluta alias labore laudantium!</p>
+      <hr />
+      <h3>Team Composition and Roles</h3>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui nobis quasi illum et expedita quia autem sequi magni impedit eligendi corrupti nemo iusto culpa consequatur, nesciunt quod suscipit adipisci, quae repudiandae! Adipisci, ratione? Similique doloribus accusamus quod porro temporibus perspiciatis id velit, molestiae, sint dolore natus soluta alias labore laudantium!</p>
+      <hr />
+      <h3>Design Methodology</h3>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui nobis quasi illum et expedita quia autem sequi magni impedit eligendi corrupti nemo iusto culpa consequatur, nesciunt quod suscipit adipisci, quae repudiandae! Adipisci, ratione? Similique doloribus accusamus quod porro temporibus perspiciatis id velit, molestiae, sint dolore natus soluta alias labore laudantium!</p>
+      <hr />
+      <h3>Schematic Design</h3>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui nobis quasi illum et expedita quia autem sequi magni impedit eligendi corrupti nemo iusto culpa consequatur, nesciunt quod suscipit adipisci, quae repudiandae! Adipisci, ratione? Similique doloribus accusamus quod porro temporibus perspiciatis id velit, molestiae, sint dolore natus soluta alias labore laudantium!</p>
+      <hr />
+      <h3>Challenges and Solutions</h3>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui nobis quasi illum et expedita quia autem sequi magni impedit eligendi corrupti nemo iusto culpa consequatur, nesciunt quod suscipit adipisci, quae repudiandae! Adipisci, ratione? Similique doloribus accusamus quod porro temporibus perspiciatis id velit, molestiae, sint dolore natus soluta alias labore laudantium!</p>
+      <hr />
+      <h3>Testing and Results</h3>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui nobis quasi illum et expedita quia autem sequi magni impedit eligendi corrupti nemo iusto culpa consequatur, nesciunt quod suscipit adipisci, quae repudiandae! Adipisci, ratione? Similique doloribus accusamus quod porro temporibus perspiciatis id velit, molestiae, sint dolore natus soluta alias labore laudantium!</p>
+      <hr />
+      <h3>Discussion</h3>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui nobis quasi illum et expedita quia autem sequi magni impedit eligendi corrupti nemo iusto culpa consequatur, nesciunt quod suscipit adipisci, quae repudiandae! Adipisci, ratione? Similique doloribus accusamus quod porro temporibus perspiciatis id velit, molestiae, sint dolore natus soluta alias labore laudantium!</p>
+      <hr />
+      <h3>Demo</h3>
+      <br />
+      <iframe width="100%" src="https://www.youtube.com/embed/9L3OatQC4fo?si=5keAzy_pDZtjnw2m" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+      <hr />
+      <h3>Acknowledgements</h3>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui nobis quasi illum et expedita quia autem sequi magni impedit eligendi corrupti nemo iusto culpa consequatur, nesciunt quod suscipit adipisci, quae repudiandae! Adipisci, ratione? Similique doloribus accusamus quod porro temporibus perspiciatis id velit, molestiae, sint dolore natus soluta alias labore laudantium!</p>
+      <hr />
+      <h3>References</h3>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui nobis quasi illum et expedita quia autem sequi magni impedit eligendi corrupti nemo iusto culpa consequatur, nesciunt quod suscipit adipisci, quae repudiandae! Adipisci, ratione? Similique doloribus accusamus quod porro temporibus perspiciatis id velit, molestiae, sint dolore natus soluta alias labore laudantium!</p>
+      <hr />
+      <h3>Appendices</h3>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui nobis quasi illum et expedita quia autem sequi magni impedit eligendi corrupti nemo iusto culpa consequatur, nesciunt quod suscipit adipisci, quae repudiandae! Adipisci, ratione? Similique doloribus accusamus quod porro temporibus perspiciatis id velit, molestiae, sint dolore natus soluta alias labore laudantium!</p>
+    </div>
+  )
+}
--- next/app/metaverse/page.tsx
+++ next/app/metaverse/page.tsx
@@ -1,44 +1,72 @@
 import Link from 'next/link'
 import s from './page.module.scss'
+import Readme from './Readme'
+import Icon from './Icon'
+import GitPubIcon from './assets/gitpub.png'
+import ReadMeIcon from './assets/readme.svg'
+import FileIcon from './assets/file.svg'
+import FolderIcon from './assets/folder.svg'
+import AnnieProf from './assets/annieProf.jpg'
+import HomeIcon from './assets/hedron.png'
 
 export default function Meta() {
   return (
     <main className={s.main}>
-      <div className={s.header}>header</div>
+      <div className={s.header}>
+        <Icon src={GitPubIcon}>Memory Traversing Machine</Icon>
+      </div>
       <div className={s.body}>
         <div className={s.left}>
           <div className={s.files}>
-            <div className={s.first}>annie he</div>
-            <div>index.html</div>
-            <div>styles.css</div>
-            <div>main.js</div>
-            <div>file5</div>
-            <div>file6</div>
+            <div className={s.first}>
+              <Icon src={AnnieProf}>Annie He</Icon>
+            </div>
+            <Icon src={FolderIcon}>file</Icon>
+            <Icon src={FileIcon}>demo.mp4</Icon>
+            <Icon src={FileIcon}>index.html</Icon>
+            <Icon src={FileIcon}>main.js</Icon>
+            <Icon src={FileIcon}>README.md</Icon>
+            <Icon src={FileIcon}>schematic.png</Icon>
+            <Icon src={FileIcon}>styles.css</Icon>
           </div>
           <div className={s.readme}>
-            <div className={s.first}>first</div>
-            <div>content</div>
+            <div className={s.first}>
+              <Icon src={ReadMeIcon}>README</Icon>
+            </div>
+            <Readme />
           </div>
         </div>
         <div className={s.right}>
           <div>
-            <div>about</div>
+            <h3>about</h3>
+            <br />
             <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro, impedit.</div>
             <hr />
           </div>
           <div>
-            <div>contributor</div>
-            <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro, impedit.</div>
+            <h3>contributor</h3>
+            <br />
+            <Icon src={AnnieProf}>Annie He</Icon>
+            <br />
+            <Icon src={HomeIcon}>Poly Hedron</Icon>
+            <br />
+            <Icon src={HomeIcon}>Noam Schuck</Icon>
+            <br />
+            <Icon src={HomeIcon}>Alexander Faust</Icon>
+            <br />
+            <Icon src={HomeIcon}>Zephaniah Odidika</Icon>
             <hr />
           </div>
           <div>
-            <div>languages</div>
+            <h3>languages</h3>
+            <br />
             <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro, impedit.</div>
           </div>
-
         </div>
       </div>
+      <div className={s.footer}>
+        <Icon src={GitPubIcon}>© 2024 GitPub, Inc.</Icon>
+      </div>
     </main>
-
   );
 }
